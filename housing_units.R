a42c3e92bfaae37d505aa4a64a16526148dd3950
Ethan Tenison <ETenison@uwatx.org>
2019-06-27 15:59:41 -0500

    updated the table headers

--- housing_units.R
+++ housing_units.R
@@ -11,10 +11,10 @@ housing_units_2013<- read.csv("./ACS_Data/ACS_13_5YR_DP04_with_ann.csv")
 housing_units_2013 <- select(housing_units_2013, GEO.id2, HC01_VC03)
 
 #2013renaming column headers and filtering for Travis country zipcodes
-housing_units_2013 <- rename(housing_units_2013, zipcode = GEO.id2, percent_housing_units = HC01_VC03)
+housing_units_2013 <- rename(housing_units_2013, zipcode = GEO.id2, housing_units = HC01_VC03)
 housing_units_2013 <- housing_units_2013[2:1942,]
 housing_units_2013 <- mutate(housing_units_2013, zipcode = as.numeric(as.character(zipcode)), 
-                             percent_housing_units = as.numeric(as.character(percent_housing_units)))
+                             housing_units = as.numeric(as.character(housing_units)))
 housing_units_2013 <- filter(housing_units_2013, zipcode %in% c( 78641,78641, 78645, 78645, 78645, 78645 ,  
                                                                  78651,78652,78653, 78660, 78669, 78669, 78691,78701, 78702,78703, 78704, 78705, 78708, 78709 ,78710, 78711
                                                                  ,78713,78714,78715,78716,78718,78719,78720,78721,78722,78723,78724,78725,78726,78727,78728,78730,78731,
@@ -27,10 +27,10 @@ housing_units_2013 <- mutate(housing_units_2013, year = 2013)
 #2014
 housing_units_2014<- read.csv("./ACS_Data/ACS_14_5YR_DP04_with_ann.csv")
 housing_units_2014 <- select(housing_units_2014, GEO.id2, HC01_VC03)
-housing_units_2014 <- rename(housing_units_2014, zipcode = GEO.id2, percent_housing_units = HC01_VC03)
+housing_units_2014 <- rename(housing_units_2014, zipcode = GEO.id2, housing_units = HC01_VC03)
 housing_units_2014 <- housing_units_2014[2:1942,]
 housing_units_2014 <- mutate(housing_units_2014, zipcode = as.numeric(as.character(zipcode)), 
-                             percent_housing_units = as.numeric(as.character(percent_housing_units)))
+                             housing_units = as.numeric(as.character(housing_units)))
 housing_units_2014 <- filter(housing_units_2014, zipcode %in% c( 78641,78641, 78645, 78645, 78645, 78645 ,  
                                                                  78651,78652,78653, 78660, 78669, 78669, 78691,78701, 78702,78703, 78704, 78705, 78708, 78709 ,78710, 78711
                                                                  ,78713,78714,78715,78716,78718,78719,78720,78721,78722,78723,78724,78725,78726,78727,78728,78730,78731,
@@ -40,10 +40,10 @@ housing_units_2014 <- mutate(housing_units_2014, year = 2014)
 #2015
 housing_units_2015<- read.csv("./ACS_Data/ACS_15_5YR_DP04_with_ann.csv")
 housing_units_2015 <- select(housing_units_2015, GEO.id2, HC01_VC03)
-housing_units_2015 <- rename(housing_units_2015, zipcode = GEO.id2, percent_housing_units = HC01_VC03)
+housing_units_2015 <- rename(housing_units_2015, zipcode = GEO.id2, housing_units = HC01_VC03)
 housing_units_2015 <- housing_units_2015[2:1942,]
 housing_units_2015 <- mutate(housing_units_2015, zipcode = as.numeric(as.character(zipcode)), 
-                             percent_housing_units = as.numeric(as.character(percent_housing_units)))
+                             housing_units = as.numeric(as.character(housing_units)))
 housing_units_2015 <- filter(housing_units_2015, zipcode %in% c( 78641,78641, 78645, 78645, 78645, 78645 ,  
                                                                  78651,78652,78653, 78660, 78669, 78669, 78691,78701, 78702,78703, 78704, 78705, 78708, 78709 ,78710, 78711
                                                                  ,78713,78714,78715,78716,78718,78719,78720,78721,78722,78723,78724,78725,78726,78727,78728,78730,78731,
@@ -53,10 +53,10 @@ housing_units_2015 <- mutate(housing_units_2015, year = 2015)
 #2016
 housing_units_2016<- read.csv("./ACS_Data/ACS_16_5YR_DP04_with_ann.csv")
 housing_units_2016 <- select(housing_units_2016, GEO.id2, HC01_VC03)
-housing_units_2016 <- rename(housing_units_2016, zipcode = GEO.id2, percent_housing_units = HC01_VC03)
+housing_units_2016 <- rename(housing_units_2016, zipcode = GEO.id2, housing_units = HC01_VC03)
 housing_units_2016 <- housing_units_2016[2:1942,]
 housing_units_2016 <- mutate(housing_units_2016, zipcode = as.numeric(as.character(zipcode)), 
-                             percent_housing_units = as.numeric(as.character(percent_housing_units)))
+                             housing_units = as.numeric(as.character(housing_units)))
 housing_units_2016 <- filter(housing_units_2016, zipcode %in% c( 78641,78641, 78645, 78645, 78645, 78645 ,  
                                                                  78651,78652,78653, 78660, 78669, 78669, 78691,78701, 78702,78703, 78704, 78705, 78708, 78709 ,78710, 78711
                                                                  ,78713,78714,78715,78716,78718,78719,78720,78721,78722,78723,78724,78725,78726,78727,78728,78730,78731,
@@ -66,10 +66,10 @@ housing_units_2016 <- mutate(housing_units_2016, year = 2016)
 #2017
 housing_units_2017<- read.csv("./ACS_Data/ACS_17_5YR_DP04_with_ann.csv")
 housing_units_2017 <- select(housing_units_2017, GEO.id2, HC01_VC03)
-housing_units_2017 <- rename(housing_units_2017, zipcode = GEO.id2, percent_housing_units = HC01_VC03)
+housing_units_2017 <- rename(housing_units_2017, zipcode = GEO.id2, housing_units = HC01_VC03)
 housing_units_2017 <- housing_units_2017[2:1942,]
 housing_units_2017 <- mutate(housing_units_2017, zipcode = as.numeric(as.character(zipcode)), 
-                             percent_housing_units = as.numeric(as.character(percent_housing_units)))
+                             housing_units = as.numeric(as.character(housing_units)))
 housing_units_2017 <- filter(housing_units_2017, zipcode %in% c( 78641,78641, 78645, 78645, 78645, 78645 ,  
                                                                  78651,78652,78653, 78660, 78669, 78669, 78691,78701, 78702,78703, 78704, 78705, 78708, 78709 ,78710, 78711
                                                                  ,78713,78714,78715,78716,78718,78719,78720,78721,78722,78723,78724,78725,78726,78727,78728,78730,78731,
