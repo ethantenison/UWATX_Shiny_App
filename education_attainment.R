967bc8febbbe3036cff80b674dcabac50435c550
Ethan Tenison <ETenison@uwatx.org>
2019-07-03 15:19:25 -0500

    created new groups for education attainment. The data set now contains 4 major variables, less than high school, high school diploma OR GED, Associates or some college, and bachelor's degree and higher.

--- education_attainment.R
+++ education_attainment.R
@@ -23,10 +23,15 @@ education_2013 <- filter(education_2013, zipcode %in% c( 78641,78641, 78645, 786
                                                          ,78713,78714,78715,78716,78718,78719,78720,78721,78722,78723,78724,78725,78726,78727,78728,78730,78731,
                                                          78732, 78733,78733, 78733, 78734,78734,78734,78734,78735, 78735,78736, 78617)) #78712 is excluded,represents UT 
 
-#Combining information to create one value for less than high school and college 
-education_2013 <- mutate(education_2013, less_than_highschool = rowSums(education_2013[,3:17]))
-                         
-                         
+#Combining information & removing unnecessary info 
+education_2013 <- mutate(education_2013, less_than_highschool = rowSums(education_2013[,3:17]),
+                         high_school_GED = estimate_total_regular_high_school_diploma + estimate_total_ged_or_alternative_credential
+                         ,associates_some_college = estimate_total_some_college_1_or_more_years_no_degree +
+                           estimate_total_some_college_less_than_1_year + estimate_total_associates_degree , bachelors_plus = 
+                           estimate_total_bachelors_degree + estimate_total_masters_degree + estimate_total_professional_school_degree +
+                           estimate_total_doctorate_degree)
+education_2013 <- education_2013[,-(3:26)]
+                
 
 #2013Adding a column for year 
 education_2013 <- mutate(education_2013, year = 2013) 
@@ -44,7 +49,13 @@ education_2014 <- filter(education_2014, zipcode %in% c( 78641,78641, 78645, 786
            78651,78652,78653, 78660, 78669, 78669, 78691,78701, 78702,78703, 78704, 78705, 78708, 78709 ,78710, 78711
            ,78713,78714,78715,78716,78718,78719,78720,78721,78722,78723,78724,78725,78726,78727,78728,78730,78731,
            78732, 78733,78733, 78733, 78734,78734,78734,78734,78735, 78735,78736, 78617)) #78712 is excluded,represents UT 
-education_2014 <- mutate(education_2014, less_than_highschool = rowSums(education_2014[,3:17]))
+education_2014 <- mutate(education_2014, less_than_highschool = rowSums(education_2014[,3:17]),
+                         high_school_GED = estimate_total_regular_high_school_diploma + estimate_total_ged_or_alternative_credential
+                         ,associates_some_college = estimate_total_some_college_1_or_more_years_no_degree +
+                           estimate_total_some_college_less_than_1_year + estimate_total_associates_degree , bachelors_plus = 
+                           estimate_total_bachelors_degree + estimate_total_masters_degree + estimate_total_professional_school_degree +
+                           estimate_total_doctorate_degree)
+education_2014 <- education_2014[,-(3:26)]
 education_2014 <- mutate(education_2014, year = 2014) 
 
 #2015
@@ -60,7 +71,13 @@ education_2015 <- filter(education_2015, zipcode %in% c( 78641,78641, 78645, 786
                                                          78651,78652,78653, 78660, 78669, 78669, 78691,78701, 78702,78703, 78704, 78705, 78708, 78709 ,78710, 78711
                                                          ,78713,78714,78715,78716,78718,78719,78720,78721,78722,78723,78724,78725,78726,78727,78728,78730,78731,
                                                          78732, 78733,78733, 78733, 78734,78734,78734,78734,78735, 78735,78736, 78617)) #78712 is excluded,represents UT 
-education_2015 <- mutate(education_2015, less_than_highschool = rowSums(education_2015[,3:17]))
+education_2015 <- mutate(education_2015, less_than_highschool = rowSums(education_2015[,3:17]),
+                         high_school_GED = estimate_total_regular_high_school_diploma + estimate_total_ged_or_alternative_credential
+                         ,associates_some_college = estimate_total_some_college_1_or_more_years_no_degree +
+                           estimate_total_some_college_less_than_1_year + estimate_total_associates_degree , bachelors_plus = 
+                           estimate_total_bachelors_degree + estimate_total_masters_degree + estimate_total_professional_school_degree +
+                           estimate_total_doctorate_degree)
+education_2015 <- education_2015[,-(3:26)]
 education_2015 <- mutate(education_2015, year = 2015)  
 
 #2016
@@ -76,7 +93,13 @@ education_2016 <- filter(education_2016, zipcode %in% c( 78641,78641, 78645, 786
                                                          78651,78652,78653, 78660, 78669, 78669, 78691,78701, 78702,78703, 78704, 78705, 78708, 78709 ,78710, 78711
                                                          ,78713,78714,78715,78716,78718,78719,78720,78721,78722,78723,78724,78725,78726,78727,78728,78730,78731,
                                                          78732, 78733,78733, 78733, 78734,78734,78734,78734,78735, 78735,78736, 78617)) #78712 is excluded,represents UT 
-education_2016 <- mutate(education_2016, less_than_highschool = rowSums(education_2016[,3:17]))
+education_2016 <- mutate(education_2016, less_than_highschool = rowSums(education_2016[,3:17]),
+                         high_school_GED = estimate_total_regular_high_school_diploma + estimate_total_ged_or_alternative_credential
+                         ,associates_some_college = estimate_total_some_college_1_or_more_years_no_degree +
+                           estimate_total_some_college_less_than_1_year + estimate_total_associates_degree , bachelors_plus = 
+                           estimate_total_bachelors_degree + estimate_total_masters_degree + estimate_total_professional_school_degree +
+                           estimate_total_doctorate_degree)
+education_2016 <- education_2016[,-(3:26)]
 education_2016 <- mutate(education_2016, year = 2016)  
 
 #2017
@@ -92,9 +115,18 @@ education_2017 <- filter(education_2017, zipcode %in% c( 78641,78641, 78645, 786
                                                          78651,78652,78653, 78660, 78669, 78669, 78691,78701, 78702,78703, 78704, 78705, 78708, 78709 ,78710, 78711
                                                          ,78713,78714,78715,78716,78718,78719,78720,78721,78722,78723,78724,78725,78726,78727,78728,78730,78731,
                                                          78732, 78733,78733, 78733, 78734,78734,78734,78734,78735, 78735,78736, 78617)) #78712 is excluded,represents UT 
-education_2017 <- mutate(education_2017, less_than_highschool = rowSums(education_2017[,3:17]))
+education_2017 <- mutate(education_2017, less_than_highschool = rowSums(education_2017[,3:17]),
+                         high_school_GED = estimate_total_regular_high_school_diploma + estimate_total_ged_or_alternative_credential
+                         ,associates_some_college = estimate_total_some_college_1_or_more_years_no_degree +
+                           estimate_total_some_college_less_than_1_year + estimate_total_associates_degree , bachelors_plus = 
+                           estimate_total_bachelors_degree + estimate_total_masters_degree + estimate_total_professional_school_degree +
+                           estimate_total_doctorate_degree)
+education_2017 <- education_2017[,-(3:26)]
 education_2017 <- mutate(education_2017, year = 2017)  
 
 #Binding the rows to create one dataset for educational_attainment 
 education <- bind_rows(education_2013, education_2014, education_2015, education_2016, education_2017)
-education <- select(education, zipcode, less_than_highschool, estimate_total_regular_high_school_diploma, estimate_total_bachelors_degree, year)
+education <- transmute(education, zipcode, estimate_total, percent_less_than_highschool = less_than_highschool/estimate_total,
+                       percent_highschool_GED = high_school_GED/estimate_total, percent_associates_somecollege = 
+                       associates_some_college/estimate_total, percent_bachelors_plus = bachelors_plus/estimate_total, 
+                       year)
