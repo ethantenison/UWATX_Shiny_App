9418f5fe260046a5c5cc3034b97f21de1204424b
Ethan Tenison <ETenison@uwatx.org>
2019-07-11 16:00:18 -0500

    demography r script

--- demography.R
+++ demography.R
@@ -21,11 +21,18 @@ demography_2013 <- filter(demography_2013, zipcode %in% c( 78617,78641, 78645, 7
                                                            78754, 78756, 78757, 78758,78759)) #78712 is excluded,represents UT 
 
 #2013selecting meaningful columns
-demography_2013 <- select(demography_2013, zipcode, 
-                          estimate_median_family_income_in_the_past_12_months_in_2013_inflation_adjusted_dollars_total)
+demography_2013 <- select(demography_2013, zipcode, contains("latino"), -(contains("margin")), -(contains("sex")))
+demography_2013 <- select(demography_2013, zipcode, percent_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_white_alone
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_black_or_african_american_alone
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_asian_alone
+                          )
 
-demography_2013 <- rename(demography_2013, median_family_income =  
-                            estimate_median_family_income_in_the_past_12_months_in_2013_inflation_adjusted_dollars_total)
+demography_2013 <- rename(demography_2013, percent_hispanic = percent_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race
+                          ,percent_white_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_white_alone
+                          ,percent_black_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_black_or_african_american_alone
+                          ,percent_asian_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_asian_alone
+                          )
 
 #2013Adding a column for year 
 demography_2013 <- mutate(demography_2013, year = 2013)  
@@ -45,12 +52,18 @@ demography_2014 <- filter(demography_2014, zipcode %in% c( 78617,78641, 78645, 7
                                                            78754, 78756, 78757, 78758,78759)) #78712 is excluded,represents UT
 
 
-demography_2014 <- select(demography_2014, zipcode, 
-                          estimate_median_family_income_in_the_past_12_months_in_2014_inflation_adjusted_dollars_total)
+demography_2014 <- select(demography_2014, zipcode, contains("latino"), -(contains("margin")), -(contains("sex")))
+demography_2014 <- select(demography_2014, zipcode, percent_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_white_alone
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_black_or_african_american_alone
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_asian_alone
+)
 
-demography_2014 <- rename(demography_2014, median_family_income =  
-                            estimate_median_family_income_in_the_past_12_months_in_2014_inflation_adjusted_dollars_total)
-
+demography_2014 <- rename(demography_2014, percent_hispanic = percent_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race
+                          ,percent_white_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_white_alone
+                          ,percent_black_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_black_or_african_american_alone
+                          ,percent_asian_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_asian_alone
+)
 
 demography_2014 <- mutate(demography_2014, year = 2014)   
 
@@ -69,11 +82,18 @@ demography_2015 <- filter(demography_2015, zipcode %in% c( 78617,78641, 78645, 7
                                                            78742, 78744, 78745, 78746, 78747, 78748, 78749, 78750, 78751, 78752, 78753, 
                                                            78754, 78756, 78757, 78758,78759)) #78712 is excluded,represents UT 
 
-demography_2015 <- select(demography_2015, zipcode, 
-                          estimate_median_family_income_in_the_past_12_months_in_2015_inflation_adjusted_dollars_total)
+demography_2015 <- select(demography_2015, zipcode, contains("latino"), -(contains("margin")), -(contains("sex")))
+demography_2015 <- select(demography_2015, zipcode, percent_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_white_alone
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_black_or_african_american_alone
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_asian_alone
+)
 
-demography_2015 <- rename(demography_2015, median_family_income =  
-                            estimate_median_family_income_in_the_past_12_months_in_2015_inflation_adjusted_dollars_total)
+demography_2015 <- rename(demography_2015, percent_hispanic = percent_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race
+                          ,percent_white_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_white_alone
+                          ,percent_black_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_black_or_african_american_alone
+                          ,percent_asian_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_asian_alone
+)
 
 demography_2015 <- mutate(demography_2015, year = 2015)   
 
@@ -93,11 +113,18 @@ demography_2016 <- filter(demography_2016, zipcode %in% c( 78617,78641, 78645, 7
                                                            78754, 78756, 78757, 78758,78759)) #78712 is excluded,represents UT
 
 
-demography_2016 <- select(demography_2016, zipcode, 
-                          estimate_median_family_income_in_the_past_12_months_in_2016_inflation_adjusted_dollars_total)
+demography_2016 <- select(demography_2016, zipcode, contains("latino"), -(contains("margin")), -(contains("sex")))
+demography_2016 <- select(demography_2016, zipcode, percent_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_white_alone
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_black_or_african_american_alone
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_asian_alone
+)
 
-demography_2016 <- rename(demography_2016, median_family_income =  
-                            estimate_median_family_income_in_the_past_12_months_in_2016_inflation_adjusted_dollars_total)
+demography_2016 <- rename(demography_2016, percent_hispanic = percent_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race
+                          ,percent_white_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_white_alone
+                          ,percent_black_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_black_or_african_american_alone
+                          ,percent_asian_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_asian_alone
+)
 
 demography_2016 <- mutate(demography_2016, year = 2016) 
 
@@ -116,12 +143,18 @@ demography_2017 <- filter(demography_2017, zipcode %in% c( 78617,78641, 78645, 7
                                                            78742, 78744, 78745, 78746, 78747, 78748, 78749, 78750, 78751, 78752, 78753, 
                                                            78754, 78756, 78757, 78758,78759)) #78712 is excluded,represents UT 
 
-demography_2017 <- select(demography_2017, zipcode, 
-                          estimate_median_family_income_in_the_past_12_months_in_2017_inflation_adjusted_dollars_total)
+demography_2017 <- select(demography_2017, zipcode, contains("latino"), -(contains("margin")), -(contains("sex")))
+demography_2017 <- select(demography_2017, zipcode, percent_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_white_alone
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_black_or_african_american_alone
+                          ,percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_asian_alone
+)
 
-demography_2017 <- rename(demography_2017, median_family_income =  
-                            estimate_median_family_income_in_the_past_12_months_in_2017_inflation_adjusted_dollars_total)
-
+demography_2017 <- rename(demography_2017, percent_hispanic = percent_hispanic_or_latino_and_race_total_population_hispanic_or_latino_of_any_race
+                          ,percent_white_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_white_alone
+                          ,percent_black_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_black_or_african_american_alone
+                          ,percent_asian_alone = percent_hispanic_or_latino_and_race_total_population_not_hispanic_or_latino_asian_alone
+)
 
 demography_2017 <- mutate(demography_2017, year = 2017)
 
