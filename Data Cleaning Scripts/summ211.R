49440ad998d2f41af32920f9dadbe089d43be59b
ethantenison <tenison.ethan@gmail.com>
2019-07-30 15:40:32 -0500

    Created a summ211 object. The old version had more information, but I'm not sure it was needed. I also changed the name of the Travis County shapefile and saved it as an R object.

---
+++ Data Cleaning Scripts/summ211.R
@@ -0,0 +1,17 @@
+#total 211 referalls
+
+data <- read_rds("./R Objects/data_4_download.RDS")
+
+summ211data <- data %>% group_by(date) %>% summarise(total_referrals = sum(total_calls))
+
+
+saveRDS(data_4_download, file = "./R Objects/summ211.RDS")
+
+#this part I might not even use 
+#housing <- read_rds("./R Objects/total_housing_units.RDS")
+
+#total_housing <- housing %>% group_by(date) %>% summarize(total_housing = sum(housing_units))
+
+#summ211data <- left_join(summ211data, total_housing, by = "date")
+
+#summ211data <- mutate(summ211data, calls_per100HH = total_calls / (housing_units/100) )
