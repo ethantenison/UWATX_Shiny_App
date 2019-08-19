9a09f128d50827dc3572d369ca60e2d261e13e8e
Ethan Tenison <ETenison@uwatx.org>
2019-08-19 17:03:34 -0500

    Major reorganization of app. The folder names were not appropriate.

--- Data Cleaning Scripts/summ211.R
+++
@@ -1,9 +0,0 @@
-#total 211 referalls
-
-data <- read_rds("./R Objects/data_4_download.RDS")
-
-summ211data <- data %>% group_by(Year) %>% summarise(total_referrals = sum(total_calls))
-
-
-saveRDS(data_4_download, file = "./R Objects/summ211.RDS")
-
